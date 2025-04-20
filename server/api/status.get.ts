2c2f1895dea43dbfefec668cc2105def48ae14fb
Pedro Falcão <22248342+HordLawk@users.noreply.github.com>
2025-04-20 13:17:18 -0300

    docs(typing): mc ping response

--- server/api/status.get.ts
+++ server/api/status.get.ts
@@ -1,5 +1,24 @@
 import protocol from 'minecraft-protocol';
 
+type Status = {
+    isModded: boolean;
+    version: {
+        name: string;
+        protocol: number;
+    };
+    players: {
+        max: number;
+        online: number;
+        sample?: {
+            name: string,
+            id: string,
+        }[];
+    };
+    description: string;
+    favicon?: string;
+    latency: number;
+}
+
 const ping = () => {
     return new Promise((resolve, reject) => {
         protocol.ping({}, (err, res) => (err ? reject(err) : resolve(res)));
@@ -7,6 +26,6 @@ const ping = () => {
 }
 
 export default defineEventHandler(async (event) => {
-    const res = await ping();
+    const res = await ping() as Status;
     return res;
 })
