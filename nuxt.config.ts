71fad9a3c3e44955a92a6b8469ee4e76eae4425d
Pedro Falcão <22248342+HordLawk@users.noreply.github.com>
2025-04-20 06:06:47 -0300

    feat: base nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/icon', '@nuxt/image'],
+  app: {
+    head: {
+      title: 'minaca', // default fallback title
+      htmlAttrs: {
+        lang: 'pt',
+      },
+    },
+  },
+  runtimeConfig: {
+    rconPassword: '',
+  },
+})
